f790fe1cbc88b44ba1231536c5c8605da3826632
TeaSippingBrit <120139450+TeaSippingBrit@users.noreply.github.com>
2023-01-18 15:09:05 +0000

    test

---
+++ fstrings.py
@@ -0,0 +1,10 @@
+variable = "world!"
+
+#This is how you would normally concatenate strings
+print("Hello",variable)
+#This is how you would concatenate strings using formatted strings
+print(f"Hello {variable}")
+#You can also perform calculations and still concatenate integers without needing to convert to strings first
+print(f"3 + 3 = {3+3}")
+#You can also multiply strings
+print(f"Hello {variable*3}")
